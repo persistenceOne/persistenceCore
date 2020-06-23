7307f61c6135cbd62b156ab0850a28f88098876a
Persistencesujith <sujith@MSI.localdomain>
2020-06-23 10:56:22 +0530

    removed the functions not in use

--- application/wasmExtension.go
+++ application/wasmExtension.go
@@ -2,11 +2,13 @@ package application
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/CosmWasm/wasmd/x/wasm"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdkTypes "github.com/cosmos/cosmos-sdk/types"
 	sdkErrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/persistenceOne/persistenceSDK/modules/assetFactory/constants"
 	"github.com/persistenceOne/persistenceSDK/modules/assetFactory/transactions/mint"
 	"github.com/persistenceOne/persistenceSDK/types"
 	"strings"
@@ -14,30 +16,27 @@ import (
 
 // this is for adding raw messages to wasm //
 
-type CustomMsg struct {
-	//	Debug string `json:"debug,omitempty"`
+type customMessage struct {
 	MsgType string          `json:"msgtype,required"`
 	Raw     json.RawMessage `json:"raw,omitempty"`
 }
 
-// Type will be assetFactory/mint , assetFactory/burn, assetFactory/Mmtate , like codec register types
+// Type will be assetFactory/mint , assetFactory/burn, assetFactory/Mmtate , (like codec register types)
 
-func wasmCustomMessageEncoder(codec *codec.Codec) *wasm.MessageEncoders {
-
+func WasmCustomMessageEncoder(codec *codec.Codec) *wasm.MessageEncoders {
 	return &wasm.MessageEncoders{
 		Custom: customEncoder(codec),
 	}
 }
 
 func customEncoder(codec *codec.Codec) wasm.CustomEncoder {
-	return func(sender sdkTypes.AccAddress, msg json.RawMessage) ([]sdkTypes.Msg, error) {
-		var customMessage CustomMsg
-		err := json.Unmarshal(msg, &customMessage)
+	return func(sender sdkTypes.AccAddress, rawMessage json.RawMessage) ([]sdkTypes.Msg, error) {
+		var customMessage customMessage
+		err := json.Unmarshal(rawMessage, &customMessage)
 		if err != nil {
 			return nil, sdkErrors.Wrap(sdkErrors.ErrJSONUnmarshal, err.Error())
 		}
 
-		fmt.Println("customMessage-MessageType: ", customMessage.MsgType, string(msg))
 		switch customMessage.MsgType {
 		case "assetFactory/mint":
 			return assetFactoryMintEncoder(codec, sender, customMessage.Raw)
@@ -46,56 +45,52 @@ func customEncoder(codec *codec.Codec) wasm.CustomEncoder {
 		case "assetFactory/burn":
 			return assetFactoryBurnEncoder(codec, sender, customMessage.Raw)
 		default:
-			return nil, sdkErrors.Wrap(wasm.ErrInvalidMsg, "Custom variant not supported in SDK- default")
+			return nil, sdkErrors.Wrap(wasm.ErrInvalidMsg, "Custom variant not supported in SDK")
 		}
-		return nil, sdkErrors.Wrap(wasm.ErrInvalidMsg, "Custom variant not supported in SDK")
 	}
 }
 
 func assetFactoryMintEncoder(codec *codec.Codec, sender sdkTypes.AccAddress, rawMessage json.RawMessage) ([]sdkTypes.Msg, error) {
 	if rawMessage != nil {
-		var astMsg AssetMintMessage
-		err := json.Unmarshal(rawMessage, &astMsg)
+		var assetMintMessage AssetMintMessage
+		err := json.Unmarshal(rawMessage, &assetMintMessage)
 		if err != nil {
 			return nil, sdkErrors.Wrap(sdkErrors.ErrJSONUnmarshal, err.Error())
 		}
-		return EncodeAssestmintMsg(sender, astMsg)
+		return encodeAssetMintMessage(sender, assetMintMessage)
 	}
-	return nil, sdkErrors.Wrap(wasm.ErrInvalidMsg, "Custom variant assetMint not supported")
+	return nil, sdkErrors.Wrap(wasm.ErrInvalidMsg, "rawMessage cannot be nil or empty")
 }
 
-func EncodeAssestmintMsg(sender sdkTypes.AccAddress, astMsg AssetMintMessage) ([]sdkTypes.Msg, error) {
+func encodeAssetMintMessage(sender sdkTypes.AccAddress, assetMintMessage AssetMintMessage) ([]sdkTypes.Msg, error) {
 
-	properties := strings.Split(astMsg.Properties, ",")
-	basePropertyList := make([]types.BaseProperty, 0)
+	properties := strings.Split(assetMintMessage.Properties, constants.PropertiesSeparator)
+	if len(properties) > constants.MaxTraitCount {
+		panic(errors.New(fmt.Sprintf("")))
+	}
+
+	var propertyList []types.Property
 	for _, property := range properties {
-		traitIDProperty := strings.Split(property, ":")
-		if len(traitIDProperty) == 2 && traitIDProperty[0] != "" {
-			basePropertyList = append(basePropertyList,
-				types.BaseProperty{
-					BaseID:   types.BaseID{IDString: traitIDProperty[0]},
-					BaseFact: types.BaseFact{BaseString: traitIDProperty[1]},
-				})
+		traitIDAndProperty := strings.Split(property, constants.TraitIDAndPropertySeparator)
+		if len(traitIDAndProperty) == 2 && traitIDAndProperty[0] != "" {
+			propertyList = append(propertyList, types.NewProperty(types.NewID(traitIDAndProperty[0]), types.NewFact(traitIDAndProperty[1], types.NewSignatures(nil))))
 		}
 	}
-	chainid := types.BaseID{IDString: astMsg.ChainID}
-	maintainersID := types.BaseID{IDString: astMsg.MaintainersID}
-	burn := types.BaseHeight{Height: astMsg.Burn}
-	lock := types.BaseHeight{Height: astMsg.Lock}
-	classificationID := types.BaseID{IDString: astMsg.ClassificationID}
-	newmg := mint.Message{ChainID: chainid, From: sender, Burn: burn, MaintainersID: maintainersID, Properties: &types.BaseProperties{BasePropertyList: basePropertyList}, ClassificationID: classificationID, Lock: lock}
-	fmt.Println(newmg, astMsg)
-	return []sdkTypes.Msg{newmg}, nil
+
+	mintMessage := mint.Message{
+		ChainID:          types.NewID(assetMintMessage.ChainID),
+		From:             sender,
+		Burn:             types.NewHeight(assetMintMessage.Burn),
+		MaintainersID:    types.NewID(assetMintMessage.MaintainersID),
+		Properties:       types.NewProperties(propertyList),
+		ClassificationID: types.NewID(assetMintMessage.ClassificationID),
+		Lock:             types.NewHeight(assetMintMessage.Lock),
+	}
+	fmt.Println(mintMessage, assetMintMessage)
+	return []sdkTypes.Msg{mintMessage}, nil
 }
 
-func assetFactoryMutateEncoder(codec *codec.Codec, sender sdkTypes.AccAddress, rawMessage json.RawMessage) ([]sdkTypes.Msg, error) {
-	return nil, sdkErrors.Wrap(wasm.ErrInvalidMsg, "Custom variant assetmutate not supported")
-}
-
-func assetFactoryBurnEncoder(codec *codec.Codec, sender sdkTypes.AccAddress, rawMessage json.RawMessage) ([]sdkTypes.Msg, error) {
-	return nil, sdkErrors.Wrap(wasm.ErrInvalidMsg, "Custom variant assetburn not supported")
-}
-
+// AssetMintMessage should look like rest request, or similar and should be convertible to sdk message
 type AssetMintMessage struct {
 	From             string `json:"from"`
 	ChainID          string `json:"chainID"`
@@ -105,63 +100,3 @@ type AssetMintMessage struct {
 	Lock             int    `json:"lock"`
 	Burn             int    `json:"burn"`
 }
-
-type Property struct {
-	ID             string     `json:"id"`
-	baseFact       string     `json:"factBytes"`
-	baseSignatures Signatures `json:"factSignatures"`
-}
-
-type Properties []Property
-
-func (p Properties) MarshalJSON() ([]byte, error) {
-	if len(p) == 0 {
-		return []byte("[]"), nil
-	}
-	var d []Property = p
-	return json.Marshal(d)
-}
-
-func (p *Properties) UnmarshalJSON(data []byte) error {
-	// make sure we deserialize [] back to null
-	if string(data) == "[]" || string(data) == "null" {
-		return nil
-	}
-	var d []Property
-	if err := json.Unmarshal(data, &d); err != nil {
-		return err
-	}
-	*p = d
-	return nil
-}
-
-type Signature struct {
-	SignatureID    string `json:"signatureID"`
-	SignatureBytes []byte `json:"signatureBytes"`
-	ValidityHeight int    `json:"validityHeight"`
-}
-
-type Signatures []Signature
-
-func (s Signatures) MarshalJSON() ([]byte, error) {
-	if len(s) == 0 {
-		return []byte("[]"), nil
-	}
-	var d []Signature = s
-	return json.Marshal(d)
-}
-
-func (s *Signatures) UnmarshalJSON(data []byte) error {
-	// make sure we deserialize [] back to null
-	if string(data) == "[]" || string(data) == "null" {
-		return nil
-	}
-	var d []Signature
-	if err := json.Unmarshal(data, &d); err != nil {
-		return err
-	}
-	*s = d
-	return nil
-}
-
-//till here
