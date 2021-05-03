5e44074e7fe196abdd14c603425724a9374a4c04
Anmol1696 <anmol1696@gmail.com>
2021-05-03 15:48:35 +0900

    Fix for #26, setup.sh to use reset script and use variable nmenoic, update dockerfile to remove final image and remove app user in docker for faster testing

--- .script/setup.sh
+++ .script/setup.sh
@@ -1,8 +1,10 @@
-./.setup.sh
+#!/bin/bash
+.script/reset.sh
+
+test_mnemonic="wage thunder live sense resemble foil apple course spin horse glass mansion midnight laundry acoustic rhythm loan scale talent push green direct brick please"
 
 persistenceCore init test --chain-id test
-persistenceCore keys add test --recover<<<"y
-wage thunder live sense resemble foil apple course spin horse glass mansion midnight laundry acoustic rhythm loan scale talent push green direct brick please"
-persistenceCore add-genesis-account test 100000000000000stake
-persistenceCore gentx test 10000000stake --chain-id test
+echo $test_mnemonic | persistenceCore keys add test --recover --keyring-backend test
+persistenceCore add-genesis-account test 100000000000000stake --keyring-backend test
+persistenceCore gentx test 10000000stake --chain-id test --keyring-backend test
 persistenceCore collect-gentxs
