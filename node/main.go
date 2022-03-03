65c75412f9488f3b460911e4757d98f691218e23
arhamchordia <chordia.1@iitj.ac.in>
2022-03-03 20:32:12 +0530

    Minor fixes

--- node/main.go
+++ node/main.go
@@ -128,19 +128,19 @@ func main() {
 	) serverTypes.Application {
 		var cache sdkTypes.MultiStorePersistentCache
 
-		if cast.ToBool(server.FlagInterBlockCache) {
+		if cast.ToBool(applicationOptions.Get(server.FlagInterBlockCache)) {
 			cache = store.NewCommitKVStoreCacheManager()
 		}
 
 		skipUpgradeHeights := make(map[int64]bool)
-		for _, h := range cast.ToIntSlice(server.FlagUnsafeSkipUpgrades) {
+		for _, h := range cast.ToIntSlice(applicationOptions.Get(server.FlagUnsafeSkipUpgrades)) {
 			skipUpgradeHeights[int64(h)] = true
 		}
 		pruningOpts, err := server.GetPruningOptionsFromFlags(applicationOptions)
 		if err != nil {
 			panic(err)
 		}
-		snapshotDir := filepath.Join(cast.ToString(flags.FlagHome), "data", "snapshots")
+		snapshotDir := filepath.Join(cast.ToString(applicationOptions.Get(flags.FlagHome)), "data", "snapshots")
 		snapshotDB, err := sdkTypes.NewLevelDB("metadata", snapshotDir)
 		if err != nil {
 			panic(err)
@@ -159,19 +159,19 @@ func main() {
 			true,
 			invalidCheckPeriod,
 			skipUpgradeHeights,
-			cast.ToString(flags.FlagHome),
+			cast.ToString(applicationOptions.Get(flags.FlagHome)),
 			applicationOptions,
 			baseapp.SetPruning(pruningOpts),
-			baseapp.SetMinGasPrices(cast.ToString(server.FlagMinGasPrices)),
-			baseapp.SetHaltHeight(cast.ToUint64(server.FlagHaltHeight)),
-			baseapp.SetHaltTime(cast.ToUint64(server.FlagHaltTime)),
-			baseapp.SetMinRetainBlocks(cast.ToUint64(server.FlagMinRetainBlocks)),
-			baseapp.SetTrace(cast.ToBool(server.FlagTrace)),
-			baseapp.SetIndexEvents(cast.ToStringSlice(server.FlagIndexEvents)),
+			baseapp.SetMinGasPrices(cast.ToString(applicationOptions.Get(server.FlagMinGasPrices))),
+			baseapp.SetHaltHeight(cast.ToUint64(applicationOptions.Get(server.FlagHaltHeight))),
+			baseapp.SetHaltTime(cast.ToUint64(applicationOptions.Get(server.FlagHaltTime))),
+			baseapp.SetMinRetainBlocks(cast.ToUint64(applicationOptions.Get(server.FlagMinRetainBlocks))),
+			baseapp.SetTrace(cast.ToBool(applicationOptions.Get(server.FlagTrace))),
+			baseapp.SetIndexEvents(cast.ToStringSlice(applicationOptions.Get(server.FlagIndexEvents))),
 			baseapp.SetInterBlockCache(cache),
 			baseapp.SetSnapshotStore(snapshotStore),
-			baseapp.SetSnapshotInterval(cast.ToUint64(server.FlagStateSyncSnapshotInterval)),
-			baseapp.SetSnapshotKeepRecent(cast.ToUint32(server.FlagStateSyncSnapshotKeepRecent)),
+			baseapp.SetSnapshotInterval(cast.ToUint64(applicationOptions.Get(server.FlagStateSyncSnapshotInterval))),
+			baseapp.SetSnapshotKeepRecent(cast.ToUint32(applicationOptions.Get(server.FlagStateSyncSnapshotKeepRecent))),
 		)
 	}
 
