a19dd997256e5cac22210da799753ca3753a086a
Anmol1696 <anmol1696@gmail.com>
2022-06-29 15:42:36 +0530

    Update local testing scripts

---
+++ contrib/local/gov_contract.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail -eu
+
+DIR="$HOME/test-contracts"
+mkdir -p $DIR
+
+echo "-----------------------"
+echo "## Add new CosmWasm contract via gov proposal"
+wget "https://github.com/CosmWasm/wasmd/raw/14688c09855ee928a12bcb7cd102a53b78e3cbfb/x/wasm/keeper/testdata/hackatom.wasm" -q -O $DIR/hackatom.wasm
+VAL1_KEY=$($CHAIN_BIN keys show -a val1)
+RESP=$($CHAIN_BIN tx gov submit-proposal wasm-store "$DIR/hackatom.wasm" \
+  --title "hackatom" \
+  --description "hackatom test contact" \
+  --deposit 10000stake \
+  --run-as $VAL1_KEY \
+  --instantiate-everybody "true" \
+  --keyring-backend test \
+  --from val1 --gas auto --fees 10000stake -y \
+  --chain-id $CHAIN_ID \
+  -b block -o json --gas-adjustment 1.5)
+echo "$RESP"
+PROPOSAL_ID=$(echo "$RESP" | jq -r '.logs[0].events[] | select(.type == "submit_proposal") | .attributes[] | select(.key == "proposal_id") | .value')
+
+echo "### Query proposal prevote"
+$CHAIN_BIN q gov proposal $PROPOSAL_ID -o json | jq
+
+echo "### Vote proposal"
+$CHAIN_BIN tx gov vote $PROPOSAL_ID yes --from val1 --yes --chain-id $CHAIN_ID \
+    --fees 500stake --gas auto --gas-adjustment 1.5 -b block -o json | jq
+$CHAIN_BIN tx gov vote $PROPOSAL_ID yes --from test1 --yes --chain-id $CHAIN_ID \
+    --fees 500stake --gas auto --gas-adjustment 1.5 -b block -o json | jq
+$CHAIN_BIN tx gov vote $PROPOSAL_ID yes --from test2 --yes --chain-id $CHAIN_ID \
+    --fees 500stake --gas auto --gas-adjustment 1.5 -b block -o json | jq
+
+echo "### Query proposal postvote"
+$CHAIN_BIN q gov proposal $PROPOSAL_ID -o json | jq
+
+echo "### Waiting for voting period..."
+sleep 40
+$CHAIN_BIN q wasm list-code -o json | jq
+
+CODE_ID=$($CHAIN_BIN q wasm list-code -o json | jq -r ".code_infos[-1].code_id")
+
+echo "-----------------------"
+echo "## Create new contract instance"
+INIT="{\"verifier\":\"$($CHAIN_BIN keys show val1 -a --keyring-backend test)\", \"beneficiary\":\"$($CHAIN_BIN keys show test1 -a)\"}"
+$CHAIN_BIN tx wasm instantiate "$CODE_ID" "$INIT" --admin="$($CHAIN_BIN keys show val1 -a --keyring-backend test)" \
+  --from val1 --amount "10000stake" --label "local0.1.0" --gas-adjustment 1.5 --fees "10000stake" \
+  --gas "auto" -y --chain-id $CHAIN_ID -b block -o json | jq
+
+CONTRACT=$($CHAIN_BIN query wasm list-contract-by-code "$CODE_ID" -o json | jq -r '.contracts[-1]')
+echo "* Contract address: $CONTRACT"
+
+echo "### Query all"
+RESP=$($CHAIN_BIN query wasm contract-state all "$CONTRACT" -o json)
+echo "$RESP" | jq
+echo "### Query smart"
+$CHAIN_BIN query wasm contract-state smart "$CONTRACT" '{"verifier":{}}' -o json | jq
+echo "### Query raw"
+KEY=$(echo "$RESP" | jq -r ".models[0].key")
+$CHAIN_BIN query wasm contract-state raw "$CONTRACT" "$KEY" -o json | jq
+
+echo "-----------------------"
+echo "## Execute contract $CONTRACT"
+MSG='{"release":{}}'
+$CHAIN_BIN tx wasm execute "$CONTRACT" "$MSG" \
+  --from val1 --gas-adjustment 1.5 --fees "10000stake" \
+  --gas "auto" -y --chain-id $CHAIN_ID -b block -o json | jq
+
+echo "-----------------------"
+echo "## Set new admin"
+echo "### Query old admin: $($CHAIN_BIN q wasm contract "$CONTRACT" -o json | jq -r '.contract_info.admin')"
+echo "### Update contract"
+$CHAIN_BIN tx wasm set-contract-admin "$CONTRACT" "$($CHAIN_BIN keys show test1 -a)" \
+  --from val1 --gas-adjustment 1.5 --gas "auto" --fees "10000stake" -y --chain-id $CHAIN_ID -b block -o json | jq
+echo "### Query new admin: $($CHAIN_BIN q wasm contract "$CONTRACT" -o json | jq -r '.contract_info.admin')"
+
---
+++ contrib/local/upgrade.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+OFFSET_HEIGHT=40
+UPGRADE_NAME=v3-wasm
+
+set -o errexit -o nounset -o pipefail -eu
+
+$CHAIN_BIN status 2>&1 | jq
+
+CURRENT_HEIGHT=$($CHAIN_BIN status 2>&1 | jq -r ".SyncInfo.latest_block_height")
+UPGRADE_HEIGHT=`expr $CURRENT_HEIGHT + $OFFSET_HEIGHT`
+echo "Starting software upgrade"
+
+echo "### Submit proposal from val1"
+RESP=$($CHAIN_BIN tx gov submit-proposal software-upgrade $UPGRADE_NAME --yes --title "$UPGRADE_NAME" --description "$UPGRADE_NAME" \
+    --upgrade-height $UPGRADE_HEIGHT --from val1 --chain-id $CHAIN_ID --deposit 100stake \
+    --fees 2000stake --gas auto --gas-adjustment 1.5 -b block -o json)
+PROPOSAL_ID=$(echo "$RESP" | jq -r '.logs[0].events[] | select(.type == "submit_proposal") | .attributes[] | select(.key == "proposal_id") | .value')
+
+echo "### Query proposal prevote"
+$CHAIN_BIN q gov proposal $PROPOSAL_ID -o json | jq
+
+echo "### Vote proposal"
+$CHAIN_BIN tx gov vote $PROPOSAL_ID yes --from val1 --yes --chain-id $CHAIN_ID \
+    --fees 200stake --gas auto --gas-adjustment 1.5 -b block -o json | jq
+$CHAIN_BIN tx gov vote $PROPOSAL_ID yes --from test1 --yes --chain-id $CHAIN_ID \
+    --fees 200stake --gas auto --gas-adjustment 1.5 -b block -o json | jq
+$CHAIN_BIN tx gov vote $PROPOSAL_ID yes --from test2 --yes --chain-id $CHAIN_ID \
+    --fees 200stake --gas auto --gas-adjustment 1.5 -b block -o json | jq
+
+echo "### Query proposal postvote"
+$CHAIN_BIN q gov proposal $PROPOSAL_ID -o json | jq
