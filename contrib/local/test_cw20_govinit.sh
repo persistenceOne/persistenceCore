588c6be9731598b7d469efe9aba993e595358782
Anmol1696 <anmol1696@gmail.com>
2022-07-22 17:17:43 +0530

    Add bash script to initiate a contract via a gov proposal, add make commands to run gov initialized contract scripts, add ports forwarding to docker container to run against local setup

--- contrib/local/test_cw20_govinit.sh
+++ contrib/local/test_cw20_govinit.sh
@@ -18,12 +18,12 @@ RESP=$($CHAIN_BIN tx gov submit-proposal wasm-store "$DIR/cw20_base.wasm" \
   --description "cw20_base contact" \
   --deposit 10000stake \
   --run-as $VAL1_KEY \
-  --instantiate-everybody "true" \
+  --instantiate-nobody "true" \
   --keyring-backend test \
   --from val1 --gas auto --fees 10000stake -y \
   --chain-id $CHAIN_ID \
   -b block -o json --gas-adjustment 1.5)
-echo "$RESP"
+echo "$RESP" | jq
 PROPOSAL_ID=$(echo "$RESP" | jq -r '.logs[0].events[] | select(.type == "submit_proposal") | .attributes[] | select(.key == "proposal_id") | .value')
 
 echo "### Query proposal prevote"
@@ -47,7 +47,7 @@ $CHAIN_BIN q wasm list-code -o json | jq
 CODE_ID=$($CHAIN_BIN q wasm list-code -o json | jq -r ".code_infos[-1].code_id")
 
 echo "-----------------------"
-echo "## Create new contract instance"
+echo "## Create new contract instance via gov"
 INIT=$(cat <<EOF
 {
   "name": "My first token",
@@ -60,9 +60,40 @@ INIT=$(cat <<EOF
 }
 EOF
 )
-$CHAIN_BIN tx wasm instantiate "$CODE_ID" "$INIT" --admin="$TEST1_KEY" \
-  --from $TEST1_KEY --amount "10000stake" --label "First Coin" --gas-adjustment 1.5 --fees "10000stake" \
-  --gas "auto" -y --chain-id $CHAIN_ID -b block -o json | jq
+echo "### Initiate contract via gov proposal"
+RESP=$($CHAIN_BIN tx gov submit-proposal instantiate-contract $CODE_ID "$INIT" \
+  --admin="$TEST1_KEY" \
+  --from $TEST1_KEY \
+  --deposit 10000stake \
+  --label "First Coin" \
+  --title "FirstCoin" \
+  --description "First cw20 token created via contract" \
+  --gas-adjustment 1.5 \
+  --fees "10000stake" \
+  --gas "auto" \
+  --run-as $VAL1_KEY \
+  -y --chain-id $CHAIN_ID -b block -o json)
+
+echo "$RESP" | jq
+PROPOSAL_ID=$(echo "$RESP" | jq -r '.logs[0].events[] | select(.type == "submit_proposal") | .attributes[] | select(.key == "proposal_id") | .value')
+
+echo "### Query proposal prevote"
+$CHAIN_BIN q gov proposal $PROPOSAL_ID -o json | jq
+
+echo "### Vote proposal"
+$CHAIN_BIN tx gov vote $PROPOSAL_ID yes --from val1 --yes --chain-id $CHAIN_ID \
+    --fees 500stake --gas auto --gas-adjustment 1.5 -b block -o json | jq
+$CHAIN_BIN tx gov vote $PROPOSAL_ID yes --from test1 --yes --chain-id $CHAIN_ID \
+    --fees 500stake --gas auto --gas-adjustment 1.5 -b block -o json | jq
+$CHAIN_BIN tx gov vote $PROPOSAL_ID yes --from test2 --yes --chain-id $CHAIN_ID \
+    --fees 500stake --gas auto --gas-adjustment 1.5 -b block -o json | jq
+
+echo "### Query proposal postvote"
+$CHAIN_BIN q gov proposal $PROPOSAL_ID -o json | jq
+
+echo "### Waiting for voting period..."
+sleep 40
+$CHAIN_BIN q wasm list-code -o json | jq
 
 CONTRACT=$($CHAIN_BIN query wasm list-contract-by-code "$CODE_ID" -o json | jq -r '.contracts[-1]')
 echo "* Contract address: $CONTRACT"
