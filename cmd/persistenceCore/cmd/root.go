387481a1826f736c05363d1241be03c952c1e096
khanh-notional <50263489+catShaark@users.noreply.github.com>
2022-10-17 14:52:23 +0700

    set srvCfg.IAVLDisableFastNode = false (#139)
    * set srvCfg.IAVLDisableFastNode = false
    
    * remove WASMConfig in initAppConfig()
    
    * remove WASMConfig in initAppConfig()
    
    * Update cmd/persistenceCore/cmd/root.go
    
    Co-authored-by: Anmol <anmol1696@gmail.com>
    
    Co-authored-by: Anmol <anmol1696@gmail.com>

--- cmd/persistenceCore/cmd/root.go
+++ cmd/persistenceCore/cmd/root.go
@@ -35,6 +35,8 @@ import (
 
 	"github.com/persistenceOne/persistenceCore/v3/app"
 	"github.com/persistenceOne/persistenceCore/v3/app/params"
+
+	serverconfig "github.com/cosmos/cosmos-sdk/server/config"
 )
 
 const flagInvalidCheckPeriod = "invalid-check-period"
@@ -74,13 +76,53 @@ func NewRootCmd() (*cobra.Command, params.EncodingConfig) {
 				return err
 			}
 
-			return server.InterceptConfigsPreRunHandler(cmd, "", nil)
+			customAppTemplate, customAppConfig := initAppConfig()
+
+			return server.InterceptConfigsPreRunHandler(cmd, customAppTemplate, customAppConfig)
 		},
 	}
 
 	initRootCmd(rootCmd, encodingConfig)
 
 	return rootCmd, encodingConfig
+}
+
+// initAppConfig helps to override default appConfig template and configs.
+// return "", nil if no custom configuration is required for the application.
+func initAppConfig() (string, interface{}) {
+	// The following code snippet is just for reference.
+
+	type CustomAppConfig struct {
+		serverconfig.Config
+	}
+
+	// Optionally allow the chain developer to overwrite the SDK's default
+	// server config.
+	srvCfg := serverconfig.DefaultConfig()
+	// The SDK's default minimum gas price is set to "" (empty value) inside
+	// app.toml. If left empty by validators, the node will halt on startup.
+	// However, the chain developer can set a default app.toml value for their
+	// validators here.
+	//
+	// In summary:
+	// - if you leave srvCfg.MinGasPrices = "", all validators MUST tweak their
+	//   own app.toml config,
+	// - if you set srvCfg.MinGasPrices non-empty, validators CAN tweak their
+	//   own app.toml to override, or use this default value.
+	//
+	// In simapp, we set the min gas prices to 0.
+	srvCfg.MinGasPrices = "0uxprt"
+	// srvCfg.BaseConfig.IAVLDisableFastNode = true // disable fastnode by default
+
+	srvCfg.IAVLDisableFastNode = false
+
+	customAppConfig := CustomAppConfig{
+		Config: *srvCfg,
+	}
+
+	customAppTemplate := serverconfig.DefaultConfigTemplate
+
+	return customAppTemplate, customAppConfig
 }
 
 // setCfg params at the package state
@@ -255,6 +297,8 @@ func (ac appCreator) newApp(
 		baseapp.SetSnapshotStore(snapshotStore),
 		baseapp.SetSnapshotInterval(cast.ToUint64(appOpts.Get(server.FlagStateSyncSnapshotInterval))),
 		baseapp.SetSnapshotKeepRecent(cast.ToUint32(appOpts.Get(server.FlagStateSyncSnapshotKeepRecent))),
+		baseapp.SetIAVLCacheSize(cast.ToInt(appOpts.Get(server.FlagIAVLCacheSize))),
+		baseapp.SetIAVLDisableFastNode(cast.ToBool(appOpts.Get(server.FlagIAVLFastNode))),
 	)
 }
 
