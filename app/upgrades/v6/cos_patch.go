ca4396b17489347b35647415977a879a014ceab1
Anmol1696 <anmol1696@gmail.com>
2022-12-19 19:51:57 +0530

    Add testing functions to emulate the mainnet

--- app/upgrades/v6/cos_patch.go
+++ app/upgrades/v6/cos_patch.go
@@ -27,14 +27,39 @@ type Validator struct {
 
 // Create new Validator vars for each validator that needs to be untombstoned
 var (
-	vals = []Validator{
-		{"HashQuark", "persistencevaloper1gydvxcnm95zwdz7h7whpmusy5d5c3ck0p9muc9", "persistencevalcons1dmjc55ve2pe537hu8h8rjrjhp4r536g5jlnlk8"},
-		{"fox99", "persistencevaloper1y2svn2zvc0puv3rx6w39aa4zlgj7qe0fz8sh6x", "persistencevalcons1ak5f5ywzmersz4z7e3nsqkem4uvf5jyya62w3c"},
-		{"Smart Stake", "persistencevaloper1qtggtsmexluvzulehxs7ypsfl82yk5aznrr2zd", "persistencevalcons1gnevun33uphh9cwkyzau5mcf0fxvuw6cyrf29g"},
-		{"Stakin", "persistencevaloper1xykmyvzk88qrlqh3wuw4jckewleyygupsumyj5", "persistencevalcons15fxjrujvsc0le9udjf63504sd4lndcam8ep4cs"},
-		{"KuCoin", "persistencevaloper18qgr8va65a50sdmp2yuy4y8w9p4pa2rf76mvmm", "persistencevalcons1m83jqu6q6aqcshnq0yjrdra9nj8rgz79mndh3j"},
-	}
+//	vals = []Validator{
+//		{"HashQuark", "persistencevaloper1gydvxcnm95zwdz7h7whpmusy5d5c3ck0p9muc9", "persistencevalcons1dmjc55ve2pe537hu8h8rjrjhp4r536g5jlnlk8"},
+//		{"fox99", "persistencevaloper1y2svn2zvc0puv3rx6w39aa4zlgj7qe0fz8sh6x", "persistencevalcons1ak5f5ywzmersz4z7e3nsqkem4uvf5jyya62w3c"},
+//		{"Smart Stake", "persistencevaloper1qtggtsmexluvzulehxs7ypsfl82yk5aznrr2zd", "persistencevalcons1gnevun33uphh9cwkyzau5mcf0fxvuw6cyrf29g"},
+//		{"Stakin", "persistencevaloper1xykmyvzk88qrlqh3wuw4jckewleyygupsumyj5", "persistencevalcons15fxjrujvsc0le9udjf63504sd4lndcam8ep4cs"},
+//		{"KuCoin", "persistencevaloper18qgr8va65a50sdmp2yuy4y8w9p4pa2rf76mvmm", "persistencevalcons1m83jqu6q6aqcshnq0yjrdra9nj8rgz79mndh3j"},
+//	}
 )
+
+func getVals(
+	ctx sdk.Context,
+	stakingKeeper *stakingkeeper.Keeper,
+) []Validator {
+	var vals []Validator
+	validators := stakingKeeper.GetAllValidators(ctx)
+	for _, validator := range validators {
+		consPubKey, _ := validator.ConsPubKey()
+		vals = append(vals, Validator{validator.Description.GetMoniker(), validator.OperatorAddress, sdk.GetConsAddress(consPubKey).String()})
+	}
+
+	return vals[len(vals)-2:]
+}
+
+func tombstoneVals(ctx sdk.Context, slashingKeeper *slashingkeeper.Keeper, vals []Validator) {
+	for _, val := range vals {
+		consAddress, _ := sdk.ConsAddressFromBech32(val.conAddress)
+
+		// Revert Tombstone info
+		signInfo, _ := slashingKeeper.GetValidatorSigningInfo(ctx, consAddress)
+		signInfo.Tombstoned = true
+		slashingKeeper.SetValidatorSigningInfo(ctx, consAddress, signInfo)
+	}
+}
 
 func mintLostTokens(
 	ctx sdk.Context,
@@ -49,6 +74,7 @@ func mintLostTokens(
 	}
 
 	for _, mintRecord := range cosMints {
+		mintRecord.Delegatee = getVals(ctx, stakingKeeper)[0].valAddress
 
 		cosValAddress, err := sdk.ValAddressFromBech32(mintRecord.Delegatee)
 		if err != nil {
@@ -66,7 +92,7 @@ func mintLostTokens(
 
 		err = mintKeeper.MintCoins(ctx, coins)
 		if err != nil {
-			panic(fmt.Sprintf("error minting %suxprt to %s: %+v", mintRecord.AmountUxprt, mintRecord.Address, err))
+			panic(fmt.Sprintf("error minting %duxprt to %s: %+v", mintRecord.AmountUxprt, mintRecord.Address, err))
 		}
 
 		delegatorAccount, err := sdk.AccAddressFromBech32(mintRecord.Address)
@@ -77,7 +103,7 @@ func mintLostTokens(
 		//TODO: The binary crashes at this point with a nil pointer dereference, needs to be fixed.
 		err = bankKeeper.SendCoinsFromModuleToAccount(ctx, minttypes.ModuleName, delegatorAccount, coins)
 		if err != nil {
-			panic(fmt.Sprintf("error sending minted %suxprt to %s: %+v", mintRecord.AmountUxprt, mintRecord.Address, err))
+			panic(fmt.Sprintf("error sending minted %duxprt to %s: %+v", mintRecord.AmountUxprt, mintRecord.Address, err))
 		}
 
 		sdkAddress, err := sdk.AccAddressFromBech32(mintRecord.Address)
@@ -87,7 +113,7 @@ func mintLostTokens(
 
 		_, err = stakingKeeper.Delegate(ctx, sdkAddress, coin.Amount, stakingtypes.Unbonded, cosValidator, true)
 		if err != nil {
-			panic(fmt.Sprintf("error delegating minted %suxprt from %s to %s: %+v", mintRecord.AmountUxprt, mintRecord.Address, mintRecord.Delegatee, err))
+			panic(fmt.Sprintf("error delegating minted %duxprt from %s to %s: %+v", mintRecord.AmountUxprt, mintRecord.Address, mintRecord.Delegatee, err))
 		}
 	}
 }
@@ -131,7 +157,10 @@ func RevertCosTombstoning(
 	bankKeeper *bankkeeper.BaseKeeper,
 	stakingKeeper *stakingkeeper.Keeper,
 ) error {
-	for _, value := range vals {
+	//for _, value := range vals {
+	tombstoneVals(ctx, slashingKeeper, getVals(ctx, stakingKeeper))
+
+	for _, value := range getVals(ctx, stakingKeeper) {
 		err := revertTombstone(ctx, slashingKeeper, value)
 		if err != nil {
 			return err
