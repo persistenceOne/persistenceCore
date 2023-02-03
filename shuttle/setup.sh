0f6ca473812d15c9d435960f43f2e6396287cb5e
Ajeet Goyal <ajeetgoyal1997@gmail.com>
2023-02-03 19:22:50 +0530

    add pre & post test scripts

--- shuttle/setup.sh
+++ shuttle/setup.sh
@@ -2,7 +2,7 @@
 
 CHAIN_BIN="${CHAIN_BIN:=persistenceCore}"
 CHAIN_DATA_DIR="${CHAIN_DATA_DIR:=.persistenceCore}"
-CHAIN_ID="${CHAIN_ID:=persistencecore-1}"
+CHAIN_ID="${CHAIN_ID:=core-1}"
 NODE_HOST="${NODE_HOST:=localhost}"
 NODE_PORT="${NODE_PORT:=26657}"
 EXPOSER_PORT="${EXPOSER_PORT:=8081}"
@@ -16,10 +16,15 @@ curl -o $MNEMONIC_CONFIG http://$NODE_HOST:$EXPOSER_PORT/keys
 echo "Starting to add keys to the keyring"
 jq -r ".genesis[0].mnemonic" $MNEMONIC_CONFIG | $CHAIN_BIN keys add $(jq -r ".genesis[0].name" $MNEMONIC_CONFIG) --recover --keyring-backend="test"
 
-# Add keys to keyringg
+# Add keys to keyring
 for ((i=0; i<$(jq -r '.validators | length' $MNEMONIC_CONFIG); i++))
 do
   jq -r ".validators[$i].mnemonic" $MNEMONIC_CONFIG | $CHAIN_BIN keys add $(jq -r ".validators[$i].name" $MNEMONIC_CONFIG) --recover --keyring-backend="test"
+done
+
+for ((i=0; i<$(jq -r '.keys | length' $MNEMONIC_CONFIG); i++))
+do
+  jq -r ".keys[$i].mnemonic" $MNEMONIC_CONFIG | $CHAIN_BIN keys add $(jq -r ".keys[$i].name" $MNEMONIC_CONFIG) --recover --keyring-backend="test"
 done
 
 echo "Update client.toml file"
---
+++ shuttle/v7/execute-existing-contract.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail -eu
+
+CHAIN_BIN="persistenceCore"
+CHAIN_ID="core-1"
+SHOW_KEY="persistenceCore keys show --keyring-backend test -a"
+
+CODE_ID=$($CHAIN_BIN q wasm list-code --chain-id $CHAIN_ID -o json | jq -r '.code_infos[-1].code_id')
+
+if [[ $CODE_ID == "null" ]]; then
+    echo "No contract found";
+    exit 1;
+fi
+
+CONTRACT_ADDR=$($CHAIN_BIN q wasm list-contract-by-code "$CODE_ID" -o json | jq -r '.contracts[-1]')
+
+echo "--------------------------------------------"
+echo "=> Query contract-state"
+echo "-> query all"
+STATE_ALL=$($CHAIN_BIN q wasm contract-state all "$CONTRACT_ADDR" -o json)
+echo "$STATE_ALL" | jq
+
+echo "-> query smart"
+$CHAIN_BIN q wasm contract-state smart "$CONTRACT_ADDR" '{"verifier":{}}' -o json | jq
+
+echo "-> query raw"
+KEY=$(echo "$STATE_ALL" | jq -r ".models[0].key")
+$CHAIN_BIN q wasm contract-state raw "$CONTRACT_ADDR" "$KEY" -o json | jq
+
+echo "--------------------------------------------"
+echo "=> Execute wasm contract: $CONTRACT_ADDR"
+MSG='{"release":{}}'
+$CHAIN_BIN tx wasm execute "$CONTRACT_ADDR" "$MSG" \
+  --from val1 --keyring-backend test --gas-adjustment 1.5 \
+  --fees "10000uxprt" --gas "auto" -y --chain-id $CHAIN_ID \
+  -b block -o json | jq -r '{height, txhash, code, raw_log}'
+
+echo "--------------------------------------------"
+echo "=> Create one more instance"
+INIT="{\"verifier\":\"$($SHOW_KEY val1)\", \"beneficiary\":\"$($SHOW_KEY test1)\"}"
+$CHAIN_BIN tx wasm instantiate "$CODE_ID" "$INIT" --admin="$($SHOW_KEY val1)" \
+  --from val1 --amount "10000uxprt" --label "local0.1.0" --gas-adjustment 1.5 --fees "10000uxprt" \
+  --gas "auto" -y --chain-id $CHAIN_ID -b block \
+  -o json | jq -r '{height, txhash, code, raw_log}'
+
+CONTRACT_ADDR=$($CHAIN_BIN query wasm list-contract-by-code "$CODE_ID" -o json | jq -r '.contracts[-1]')
+echo "-> New instantiated contract address: $CONTRACT_ADDR"
+
+echo "--------------------------------------------"
+echo "=> Execute wasm contract: $CONTRACT_ADDR"
+MSG='{"release":{}}'
+$CHAIN_BIN tx wasm execute "$CONTRACT_ADDR" "$MSG" \
+  --from val1 --keyring-backend test --gas-adjustment 1.5 \
+  --fees "10000uxprt" --gas "auto" -y --chain-id $CHAIN_ID \
+  -b block -o json | jq -r '{height, txhash, code, raw_log}'
+
+echo "-------------------DONE---------------------"
