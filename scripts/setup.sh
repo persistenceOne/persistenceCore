37cd240147f50b3b8be4539c240a0827de77df9d
Tikaryan <aryantikarya9@gmail.com>
2023-03-15 13:55:30 +0530

    Address comments

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,6 @@ test_mnemonic="wage thunder live sense resemble foil apple course spin horse gla
 
 persistenceCore init test --chain-id test
 echo $test_mnemonic | persistenceCore keys add test --recover --keyring-backend test
-persistenceCore add-genesis-account test 100000000000000uxprt 100000000000000stake --keyring-backend test
-persistenceCore gentx test 10000000uxprt 10000000stake --chain-id test --keyring-backend test
+persistenceCore add-genesis-account test 100000000000000uxprt,100000000000000stake --keyring-backend test
+persistenceCore gentx test 10000000uxprt --chain-id test --keyring-backend test
 persistenceCore collect-gentxs
