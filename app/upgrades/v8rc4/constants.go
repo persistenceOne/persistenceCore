bb4084b2277cf4a21c1fd2fad2a729beee51ba77
Max <maksim@persistence.one>
2023-05-11 13:59:24 +0200

    chore: prepare v8-rc4 upgrade

---
+++ app/upgrades/v8rc4/constants.go
@@ -0,0 +1,20 @@
+package v8rc4
+
+import (
+	store "github.com/cosmos/cosmos-sdk/store/types"
+
+	"github.com/persistenceOne/persistenceCore/v8/app/upgrades"
+)
+
+const (
+	// UpgradeName defines the on-chain upgrade name.
+	UpgradeName = "v8-rc4"
+)
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName:          UpgradeName,
+	CreateUpgradeHandler: CreateUpgradeHandler,
+	StoreUpgrades: store.StoreUpgrades{
+		Added: []string{},
+	},
+}
