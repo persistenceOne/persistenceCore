4cdab02f28d4d8408b961ff90434ce523509af34
Ajeet Goyal <ajeetgoyal1997@gmail.com>
2023-12-20 19:20:39 +0530

    v11.1.0 upgrade handler

---
+++ app/upgrades/v11.1.0/constants.go
@@ -0,0 +1,20 @@
+package v11_1_0
+
+import (
+	store "github.com/cosmos/cosmos-sdk/store/types"
+
+	"github.com/persistenceOne/persistenceCore/v11/app/upgrades"
+)
+
+const (
+	// UpgradeName defines the on-chain upgrade name.
+	UpgradeName = "v11.1.0"
+)
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName:          UpgradeName,
+	CreateUpgradeHandler: CreateUpgradeHandler,
+	StoreUpgrades: store.StoreUpgrades{
+		Added: []string{},
+	},
+}
---
+++ app/upgrades/v11.1.0/upgrades.go
@@ -0,0 +1,17 @@
+package v11_1_0
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+
+	"github.com/persistenceOne/persistenceCore/v11/app/upgrades"
+)
+
+func CreateUpgradeHandler(args upgrades.UpgradeHandlerArgs) upgradetypes.UpgradeHandler {
+	return func(ctx sdk.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		ctx.Logger().Info("running upgrade handler")
+
+		return args.ModuleManager.RunMigrations(ctx, args.Configurator, vm)
+	}
+}
