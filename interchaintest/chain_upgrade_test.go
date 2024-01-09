59e851fdb1163eddc78a8fe6031a0b7203f4fcbe
kruspy <krusspy@gmail.com>
2024-01-09 19:42:48 +0100

    typo

--- interchaintest/chain_upgrade_test.go
+++ interchaintest/chain_upgrade_test.go
@@ -30,7 +30,7 @@ func TestPersistenceUpgradeBasic(t *testing.T) {
 	var (
 		chainName            = "persistence"
 		upgradeRepo          = PersistenceCoreImage.Repository
-		initialVersion       = "v11.0.0"
+		initialVersion       = "v11.1.0"
 		upgradeBranchVersion = PersistenceCoreImage.Version
 		upgradeName          = "v11.2.0"
 	)
