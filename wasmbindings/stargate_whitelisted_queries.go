2a83e7e55bba4b422a34ad72545a90354c5dcb4a
Nabarun Sarkar <nabsarkar@gmail.com>
2024-01-10 18:48:33 +0400

    chore(wasmbindings): Add more queries to stargate whitelist (#278)
    Update stargate_whitelisted_queries.go

--- wasmbindings/stargate_whitelisted_queries.go
+++ wasmbindings/stargate_whitelisted_queries.go
@@ -7,6 +7,7 @@ import (
 	wasmvmtypes "github.com/CosmWasm/wasmvm/types"
 	"github.com/cosmos/cosmos-sdk/codec"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
+	ibctransfertypes "github.com/cosmos/ibc-go/v7/modules/apps/transfer/types"
 	oracletypes "github.com/persistenceOne/persistence-sdk/v2/x/oracle/types"
 	liquidstaketypes "github.com/persistenceOne/pstake-native/v2/x/liquidstake/types"
 	liquidstakeibctypes "github.com/persistenceOne/pstake-native/v2/x/liquidstakeibc/types"
@@ -16,18 +17,22 @@ import (
 var stargateWhitelistQueries sync.Map
 
 func init() {
-	// stargate queries available for the contract
+	// oracle
 	setStargateWhitelistQuery("/persistence.oracle.v1beta1.Query/ExchangeRate", &oracletypes.QueryExchangeRateResponse{})
 
-	// governance module
+	// governance
 	setStargateWhitelistQuery("/cosmos.gov.v1.Query/Proposal", &govtypes.QueryProposalResponse{})
 	setStargateWhitelistQuery("/cosmos.gov.v1.Query/Proposals", &govtypes.QueryProposalsResponse{})
 	setStargateWhitelistQuery("/cosmos.gov.v1.Query/Deposit", &govtypes.QueryDepositResponse{})
 	setStargateWhitelistQuery("/cosmos.gov.v1.Query/Params", &govtypes.QueryParamsResponse{})
 
-	// liquid staking module for exchange rate query from a contract
+	// liquid staking
 	setStargateWhitelistQuery("/pstake.liquidstakeibc.v1beta1.Query/ExchangeRate", &liquidstakeibctypes.QueryExchangeRateResponse{})
+	setStargateWhitelistQuery("/pstake.liquidstakeibc.v1beta1.Query/HostChains", &liquidstakeibctypes.QueryHostChainsResponse{})
 	setStargateWhitelistQuery("/pstake.liquidstake.v1beta1.Query/States", &liquidstaketypes.QueryStatesResponse{})
+
+	// ibc
+	setStargateWhitelistQuery("/ibc.applications.transfer.v1.Query/DenomTrace", &ibctransfertypes.QueryDenomTraceResponse{})
 }
 
 // setStargateWhitelistQuery stores the stargate queries.
