7b9eaea5f14c00af39e3d76dbc95d5e0dbbb4c0d
Marc Puig <krusspy@gmail.com>
2024-01-26 17:04:13 +0100

    chore: Testnet fix upgrade (#282)
    * migration
    
    * pstake bump
    
    * add default

--- interchaintest/chain_upgrade_test.go
+++ interchaintest/chain_upgrade_test.go
@@ -30,9 +30,9 @@ func TestPersistenceUpgradeBasic(t *testing.T) {
 	var (
 		chainName            = "persistence"
 		upgradeRepo          = PersistenceCoreImage.Repository
-		initialVersion       = "v11.2.0"
+		initialVersion       = "v11.3.0"
 		upgradeBranchVersion = PersistenceCoreImage.Version
-		upgradeName          = "v11.3.0"
+		upgradeName          = "v11.4.0"
 	)
 
 	CosmosChainUpgradeTest(
