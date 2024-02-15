89839b5f91d02b287d47b914c6836aaf7771c06d
Ajeet Goyal <ajeetgoyal1997@gmail.com>
2024-02-15 19:09:35 +0530

    add upgrade handler for 10.4.1

---
+++ app/upgrades/v10.4.1/constant.go
@@ -0,0 +1,20 @@
+package v10_4_1
+
+import (
+	store "github.com/cosmos/cosmos-sdk/store/types"
+
+	"github.com/persistenceOne/persistenceCore/v10/app/upgrades"
+)
+
+const (
+	// UpgradeName defines the on-chain upgrade name.
+	UpgradeName = "v10.4.1"
+)
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName:          UpgradeName,
+	CreateUpgradeHandler: CreateUpgradeHandler,
+	StoreUpgrades: store.StoreUpgrades{
+		Added: []string{},
+	},
+}
