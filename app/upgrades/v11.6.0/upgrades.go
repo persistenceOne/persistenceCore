597fa2fd8a85d5c96d2cf69bce4ba692e72209f1
puneetmahajan <puneet@persistence.one>
2024-02-21 18:45:10 +0530

    add upgrades for v11.6

---
+++ app/upgrades/v11.6.0/upgrades.go
@@ -0,0 +1,16 @@
+package v11_5_0
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+
+	"github.com/persistenceOne/persistenceCore/v11/app/upgrades"
+)
+
+func CreateUpgradeHandler(args upgrades.UpgradeHandlerArgs) upgradetypes.UpgradeHandler {
+	return func(ctx sdk.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		ctx.Logger().Info("running upgrade handler")
+		return args.ModuleManager.RunMigrations(ctx, args.Configurator, vm)
+	}
+}
