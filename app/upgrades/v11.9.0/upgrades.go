33534cb858cf402070703ea36da94be182be58c4
puneetmahajan <puneet@persistence.one>
2024-04-09 20:04:32 +0530

    code for v11.9.0

---
+++ app/upgrades/v11.9.0/upgrades.go
@@ -0,0 +1,24 @@
+package v11_9_0
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+	packetforwardtypes "github.com/cosmos/ibc-apps/middleware/packet-forward-middleware/v7/packetforward/types"
+
+	"github.com/persistenceOne/persistenceCore/v11/app/upgrades"
+)
+
+func CreateUpgradeHandler(args upgrades.UpgradeHandlerArgs) upgradetypes.UpgradeHandler {
+	return func(ctx sdk.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		ctx.Logger().Info("running module migrations...")
+
+		// register param subspace in order to migrate from this legacy params.
+		subspace, ok := args.Keepers.ParamsKeeper.GetSubspace(packetforwardtypes.ModuleName)
+		if ok && !subspace.HasKeyTable() {
+			subspace.WithKeyTable(packetforwardtypes.ParamKeyTable())
+		}
+
+		return args.ModuleManager.RunMigrations(ctx, args.Configurator, vm)
+	}
+}
