8476aa4c0da268b36b61ce015a17b594a440696d
Puneet <59960662+puneet2019@users.noreply.github.com>
2025-06-10 23:19:51 +0400

    feat: prepare testnet v11.21.0-rc0 upgrade (#378)
    add code for stkassets redemption for testnet

--- app/app.go
+++ app/app.go
@@ -56,13 +56,13 @@ import (
 
 	"github.com/persistenceOne/persistenceCore/v11/app/keepers"
 	"github.com/persistenceOne/persistenceCore/v11/app/upgrades"
-	v11_17_0 "github.com/persistenceOne/persistenceCore/v11/app/upgrades/v11.17.0"
+	v11210rc0 "github.com/persistenceOne/persistenceCore/v11/app/upgrades/testnet/v11.21.0-rc0"
 	"github.com/persistenceOne/persistenceCore/v11/client/docs"
 )
 
 var (
 	DefaultNodeHome string
-	Upgrades        = []upgrades.Upgrade{v11_17_0.Upgrade}
+	Upgrades        = []upgrades.Upgrade{v11210rc0.Upgrade}
 	ModuleBasics    = module.NewBasicManager(keepers.AppModuleBasics...)
 )
 
