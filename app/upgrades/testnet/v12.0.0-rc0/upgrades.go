149c6662840ab49847cd5397a3619b3b7d100d29
puneetmahajan <puneetmahajan1996@gmail.com>
2025-06-23 19:46:04 +0400

    add upgrade to FixLSMData.

--- app/upgrades/testnet/v12.0.0-rc0/upgrades.go
+++ app/upgrades/testnet/v12.0.0-rc0/upgrades.go
@@ -10,7 +10,38 @@ import (
 func CreateUpgradeHandler(args upgrades.UpgradeHandlerArgs) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		ctx.Logger().Info("running module migrations...")
-
+		err := FixLSMData(ctx, args)
+		if err != nil {
+			return vm, err
+		}
 		return args.ModuleManager.RunMigrations(ctx, args.Configurator, vm)
 	}
+}
+
+func FixLSMData(ctx sdk.Context, args upgrades.UpgradeHandlerArgs) error {
+	k := args.Keepers.StakingKeeper
+	err := k.RefreshTotalLiquidStaked(ctx)
+	if err != nil {
+		return err
+	}
+	for _, validator := range k.GetAllValidators(ctx) {
+		validator.ValidatorBondShares = sdk.ZeroDec()
+		k.SetValidator(ctx, validator)
+	}
+
+	// Sum up the total liquid tokens and increment each validator's liquid shares
+	for _, delegation := range k.GetAllDelegations(ctx) {
+		delegatorAddress, err := sdk.AccAddressFromBech32(delegation.DelegatorAddress)
+		if err != nil {
+			return err
+		}
+
+		// disable valbond on delegator level.
+		if k.DelegatorIsLiquidStaker(delegatorAddress) {
+			delegation.ValidatorBond = false
+			k.SetDelegation(ctx, delegation)
+		}
+	}
+
+	return nil
 }
