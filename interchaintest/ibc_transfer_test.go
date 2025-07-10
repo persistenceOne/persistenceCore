eb56accea1b97067823b93fd423f0d6f227557d7
puneetmahajan <puneetmahajan1996@gmail.com>
2025-07-10 11:12:35 +0400

    update gaia version

--- interchaintest/ibc_transfer_test.go
+++ interchaintest/ibc_transfer_test.go
@@ -38,7 +38,7 @@ func TestPersistenceGaiaIBCTransfer(t *testing.T) {
 		},
 		{
 			Name:          "gaia",
-			Version:       "v24.0.0",
+			Version:       "v14.1.0",
 			NumValidators: &numVals,
 			NumFullNodes:  &numFullNodes,
 		},
