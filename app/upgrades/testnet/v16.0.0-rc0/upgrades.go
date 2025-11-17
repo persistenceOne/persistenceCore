f602aa6a759ec70a68ea6be427cca86aea3d119b
puneetmahajan <puneetmahajan1996@gmail.com>
2025-11-17 12:22:20 +0530

    add testnet upgrade instructions

--- app/upgrades/testnet/v16.0.0-rc0/upgrades.go
+++ app/upgrades/testnet/v16.0.0-rc0/upgrades.go
@@ -2,10 +2,14 @@ package v16_0_0_rc0
 
 import (
 	"context"
+	"fmt"
 
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	ibctransfertypes "github.com/cosmos/ibc-go/v10/modules/apps/transfer/types"
+
+	"github.com/persistenceOne/persistenceCore/v16/app/keepers"
 	"github.com/persistenceOne/persistenceCore/v16/app/upgrades"
 )
 
@@ -20,7 +24,29 @@ func CreateUpgradeHandler(args upgrades.UpgradeHandlerArgs) upgradetypes.Upgrade
 			return vm, err
 		}
 
+		err = ResetIBCTransferVersions(sdkCtx, args.Keepers)
+		if err != nil {
+			return vm, err
+		}
+
 		sdkCtx.Logger().Info("Upgrade complete")
 		return vm, nil
 	}
+}
+
+func ResetIBCTransferVersions(sdkCtx sdk.Context, keepers *keepers.AppKeepers) error {
+
+	channels := keepers.IBCKeeper.ChannelKeeper.GetAllChannels(sdkCtx)
+	for _, channel := range channels {
+		if channel.PortId == ibctransfertypes.PortID && channel.Version != ibctransfertypes.V1 {
+			channelDb, ok := keepers.IBCKeeper.ChannelKeeper.GetChannel(sdkCtx, channel.PortId, channel.ChannelId)
+			if !ok {
+				return fmt.Errorf("channel %s not found", channel.ChannelId)
+			}
+			channelDb.Version = ibctransfertypes.V1
+			keepers.IBCKeeper.ChannelKeeper.SetChannel(sdkCtx, channel.PortId, channel.ChannelId, channelDb)
+		}
+	}
+
+	return nil
 }
